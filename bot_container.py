b3e35833aaa78df9a9d7411863f0aa747f9d30b0
Ivan Nikola <nikolaivan@mail.ru>
2020-09-22 00:45:35 +0300

    debugging bot_logic.answer

--- bot_container.py
+++ bot_container.py
@@ -34,7 +34,7 @@ class BotContainer:
         self._dp.add_handler(CommandHandler("stop", self.logic.stop))
         self._dp.add_handler(CommandHandler("help", self.logic.help))
         self._dp.add_handler(CommandHandler("hint", self.logic.hint))
-        self._dp.add_handler(MessageHandler(Filters.text, self.logic.answer))
+        self._dp.add_handler(MessageHandler(Filters.text & (~Filters.command), self.logic.answer))
         self._dp.add_error_handler(self.logic.error)
 
 
